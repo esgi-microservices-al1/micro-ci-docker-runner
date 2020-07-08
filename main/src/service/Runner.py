333260e0e61438c1fcb227402c7ebd75f51f572a
Patrick ZHUANG <patrickzhuang@hotmail.fr>
2020-07-08 19:55:36 +0200

    API

--- main/src/service/Runner.py
+++ main/src/service/Runner.py
@@ -1,13 +1,16 @@
 import docker
 from docker.types import Mount
-
+from src.service.StatusService import *
 
 class Runner:
     def __init__(self, path):
-        client = docker.from_env()
-        image, logs = client.images.build(path=path, dockerfile='Dockerfile')
+        self.statusService = StatusService()
+
+        self.client = docker.from_env()
+        self.image, logs = self.client.images.build(path=f'/projects/{path}', dockerfile='Dockerfile')
         mounts = [Mount(target="/var/run/docker.sock", source="/var/run/docker.sock", type='bind')]
-        self.container = client.containers.run(image=image.id, tty=True, detach=True, mounts=mounts)
+        self.container = self.client.containers.run(image=self.image.id, tty=True, detach=True, mounts=mounts)
+        self.statusService.add_image_ids(self.image.id, self.container.id, path)
 
         for logLine in logs:
             print(logLine)
@@ -20,3 +23,6 @@ class Runner:
     def stop(self):
         self.container.stop()
         self.container.remove()
+        image_id = self.image.id
+        self.client.images.remove(self.image.id)
+        self.statusService.delete_by_image_id(image_id)
