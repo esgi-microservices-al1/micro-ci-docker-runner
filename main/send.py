c8506e20acd4f2b1ebd409609ed39db29577ea8e
Desjardins Alexis <vizuar.gamer@gmail.com>
2020-07-12 23:45:13 +0200

    celui-là plutôt

--- main/send.py
+++ main/send.py
@@ -1,19 +1,6 @@
 from src.broker.message import Message
 
 msg = Message()
-msg_out = {
-    'process_id': 1,
-    'command': "ls",
-    'output': {
-        'stdout': '',
-        'stderr': '',
-        'status': 'success'
-    },
-    'status': 6
-}
-msg.send(msg_out)
-
-
 for i in range(1, 6):
     msg.sendToUs('''
         {
