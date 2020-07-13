f82e79e5b173cf187e312c08eb1ec0c5b09a479c
Patrick ZHUANG <patrickzhuang@hotmail.fr>
2020-07-13 12:01:50 +0200

    Prepare for threading

--- main/src/service/ApiService.py
+++ main/src/service/ApiService.py
@@ -6,6 +6,7 @@ import datetime
 from flask import Flask
 from flask_cors import CORS
 from src.service.StatusService import *
+from time import sleep
 
 app = Flask(__name__)
 CORS(app)
@@ -16,10 +17,12 @@ def getStats():
     responses = {'datas': []}
     client = docker.APIClient()
     statusService = StatusService()
+    while StatusService.inUse:
+        sleep(0.10)
     statusService.read()
     for image_container_id in StatusService.image_container_ids:
         image2 = client.inspect_image(image_container_id[0])
-        created_base = image2.get('Created')[:-4]+'Z'
+        created_base = image2.get('Created')[:-4] + 'Z'
         created = datetime.datetime.strptime(created_base, "%Y-%m-%dT%H:%M:%S.%fZ") + datetime.timedelta(hours=2)
         difference = datetime.datetime.now() - created
         response = {'image_id': image_container_id[0], 'container_id': image_container_id[1],
--- main/src/service/StatusService.py
+++ main/src/service/StatusService.py
@@ -3,12 +3,14 @@ import csv
 
 class StatusService:
     image_container_ids = []
+    inUse = False
 
     def __init__(self):
         # open('resources/data.csv', 'w+')
         print("init status service")
 
     def read(self):
+        StatusService.inUse = True
         StatusService.image_container_ids = []
         with open('/opt/project/main/resources/data.csv', mode='r') as csv_file:
             csv_reader = csv.reader(csv_file, delimiter=';')
@@ -20,23 +22,30 @@ class StatusService:
                     line_count += 1
             print(f'Processed {line_count} lines.')
             csv_file.close()
+        StatusService.inUse = False
 
     def write(self):
+        StatusService.inUse = True
         with open('/opt/project/main/resources/data.csv', mode='w', newline='') as csv_file:
             csv_writer = csv.writer(csv_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
             for image_container_id in StatusService.image_container_ids:
                 csv_writer.writerow([image_container_id[0], image_container_id[1], image_container_id[2]])
             csv_file.close()
+        StatusService.inUse = False
 
     def add_image_ids(self, image_id, container_id, project_id):
         StatusService.image_container_ids.append((image_id, container_id, project_id))
+        while StatusService.inUse:
+            sleep(0.10)
         self.write()
 
     def delete_by_image_id(self, container_id):
         for image_container_id in StatusService.image_container_ids:
             if image_container_id[0] == container_id:
                 StatusService.image_container_ids.remove(image_container_id)
+        while StatusService.inUse:
+            sleep(0.10)
         self.write()
 
     def checkIfOtherImage(self, image_id):
